dfe5c7a07c530836a633a3a7c0143b1e2bbd03d9
kirill <kirill@DESKTOP-Q8RV46M>
2020-01-18 14:29:23 +0300

    add view helper with modal window engine

---
+++ ViewModels/Helper/ViewHelper.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace TSensor.Web.ViewModels.Helper
+{
+    public static class ViewHelper
+    {
+        public static HtmlString ValidState(this IHtmlHelper html, string field)
+        {
+            return new HtmlString(
+                html?.ViewData?.ModelState[field]?.ValidationState == ModelValidationState.Invalid ?
+                    " has-error" : null);
+        }
+
+        public static HtmlString ViewModelWindow(this IHtmlHelper html)
+        {
+            var viewModel = html?.ViewData?.Model as ViewModelBase;
+            if (viewModel?.IsError == true)
+            {
+                return new HtmlString($@"
+<script>
+    new ErrorModal('{viewModel.ErrorMessage}').show();
+</script>");
+            }
+            else if (viewModel?.IsSuccess == true)
+            {
+                return new HtmlString($@"
+<script>
+    new SuccessModal('{viewModel?.SuccessMessage}').show();
+</script>");
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static HtmlString MenuPage(this IHtmlHelper html, string controllerName)
+        {
+            var routeData = html?.ViewContext.RouteData.Values;
+
+            if (
+                routeData?.ContainsKey("controller") == true &&
+                routeData["controller"] as string == controllerName)
+            {
+                return new HtmlString("class=\"active\"");
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ wwwroot/js/main.js
@@ -0,0 +1,22 @@
+﻿var Modal = function (type, header, message) {
+    $(
+        '<div class="modal fade" tabindex="-1"><div class="modal-dialog"><div class="modal-content">' +
+        '<div class="modal-header ' + type + '"><h4 class="modal-title">' + header + '</h4></div>' +
+        '<div class="modal-body"><p>' + message + '</p></div>' +
+        '<div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button></div>' +
+        '</div></div></div>').appendTo($('body'));
+
+    this.show = function () {
+        $('.modal').modal();
+    };
+
+    return this;
+};
+
+var ErrorModal = function (message) {
+    return new Modal('alert-error', 'Ошибка', message);
+};
+
+var SuccessModal = function (message) {
+    return new Modal('alert-success', 'Успешно', message);
+};
