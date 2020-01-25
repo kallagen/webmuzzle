4c4dd07398c26efe77b892abe0750f8af353a13c
kirill <kirill@DESKTOP-Q8RV46M>
2020-01-25 17:36:32 +0300

    add locker to filelogservice (no more locks?)

--- Models/Services/Log/FileLogService.cs
+++ Models/Services/Log/FileLogService.cs
@@ -2,11 +2,15 @@
 using Microsoft.Extensions.Configuration;
 using System;
 using System.IO;
+using System.Threading;
 
 namespace TSensor.Web.Models.Services.Log
 {
     public class FileLogService
     {
+        private static ReaderWriterLock InputErrorLocker = new ReaderWriterLock();
+        private static ReaderWriterLock RawInputLocker = new ReaderWriterLock();
+
         private readonly string LogFilePath;
 
         public FileLogService(IConfiguration configuration, IWebHostEnvironment environment)
@@ -16,14 +20,41 @@ namespace TSensor.Web.Models.Services.Log
 
         public void Write(string header, string message)
         {
-            var path = $"{LogFilePath}\\{header}";
-            if (!Directory.Exists(path))
+            switch (header)
             {
-                Directory.CreateDirectory(path);
+                case "inputerror":
+                    Write(header, message, InputErrorLocker);
+                    break;
+                case "rawinput":
+                    Write(header, message, RawInputLocker);
+                    break;
+                default: break;
             }
+        }
 
-            using var writer = File.AppendText($"{path}\\{DateTime.Now.ToString("yyyyMMdd")}.log");
-            writer.Write($"{DateTime.Now.ToString("HH:mm:ss")}: {message}");
+        private void Write(string header, string message, ReaderWriterLock locker)
+        {
+            try
+            {
+                locker.AcquireWriterLock(5000);
+
+                try
+                {
+                    var path = $"{LogFilePath}\\{header}";
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+
+                    using var writer = File.AppendText($"{path}\\{DateTime.Now.ToString("yyyyMMdd")}.log");
+                    writer.Write($"{DateTime.Now.ToString("HH:mm:ss")}: {message}");
+                }
+                finally
+                {
+                    locker.ReleaseWriterLock();
+                }
+            }
+            catch { }
         }
     }
 }
