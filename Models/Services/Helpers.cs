9140574dd87c74d441c42401135fd9ab1c6c34c1
kirill <kirill@DESKTOP-Q8RV46M>
2020-01-26 11:10:01 +0300

    add date to js ticks helper

---
+++ Models/Services/Helpers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TSensor.Web.Models.Services
+{
+    public static class Helpers
+    {
+        public static long TicksJs(this DateTime date)
+        {
+            return (date.Ticks - 621355968000000000) / 10000;
+        }
+    }
+}
