28a9cf0c42d3bce5e3d6ad75acff9a63eb61a9a2
kirill <kirill.s.nikolaev@gmail.com>
2020-02-16 14:44:14 +0300

    add partial view for point menu with tank list

---
+++ ViewModels/Point/MenuElementViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TSensor.Web.ViewModels.Point
+{
+    public class MenuElementViewModel
+    {
+        public Models.Entity.Point Point { get; set; }
+        public Guid? GroupGuid { get; set; }
+    }
+}
