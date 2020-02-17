35440025a96da15574c600c8a5f8830ba73ff468
kirill <kirill.s.nikolaev@gmail.com>
2020-02-17 18:57:56 +0300

    add favorite menu viewcomponent

---
+++ ViewComponents/FavoriteMenu.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TSensor.Web.Models.Repository;
+using TSensor.Web.Models.Services.Security;
+
+namespace TSensor.Web.ViewComponents
+{
+    public class FavoriteMenu : ViewComponent
+    {
+        private readonly IFavoriteRepository _favoriteRepository;
+        private readonly AuthService _authService;
+
+        public FavoriteMenu(IFavoriteRepository favoriteRepository, AuthService authService)
+        {
+            _favoriteRepository = favoriteRepository;
+            _authService = authService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var favoriteList = _favoriteRepository.ListByUser(_authService.CurrentUserGuid);
+
+            return View(favoriteList);
+        }
+    }
+}
