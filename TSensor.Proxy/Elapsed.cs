eb9cb335c8a15a4da4524b7cefe2b101353acb86
kirill <kirill.s.nikolaev@gmail.com>
2020-03-07 08:03:49 +0300

    add elapsed class for elapsed time measuring

---
+++ TSensor.Proxy/Elapsed.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TSensor.Proxy
+{
+    public class Elapsed : IDisposable
+    {
+        private DateTime? start = DateTime.Now;
+
+        public static Elapsed Create =>
+            new Elapsed();
+
+        public override string ToString()
+        {
+            return Math.Round((DateTime.Now - start.Value).TotalMilliseconds, 0, MidpointRounding.ToPositiveInfinity)
+                .ToString();
+        }
+
+        public void Dispose()
+        {
+            start = null;
+        }
+    }
+}
