31b29f33fa67acf53e8021ee513c28fb77820858
kirill <kirill.s.nikolaev@gmail.com>
2020-03-07 08:42:14 +0300

    add httpresult with exception; add thread-safe isconnectionerror flag

--- TSensor.Proxy/Http.cs
+++ TSensor.Proxy/Http.cs
@@ -10,29 +10,71 @@ namespace TSensor.Proxy
 {
     public static class Http
     {
-        public static async Task<string> PostAsync(string url, Dictionary<string, string> param)
+        private static readonly object Locker = new object();
+
+        private static bool isConnectionError = false;
+        public static bool IsConnectionError
         {
-            var request = WebRequest.Create(url) as HttpWebRequest;
-            request.Method = "POST";
-            request.ContentType = "application/x-www-form-urlencoded";
-            request.Timeout = 30000;
-            request.MaximumAutomaticRedirections = 5;
-            request.AllowAutoRedirect = true;
-            request.Headers.Add("Upgrade-Insecure-Requests", "1");
-
-            var body = string.Join("&", param.Select(p => $"{p.Key}={Uri.EscapeDataString(p.Value)}"));
-            var postBytes = Encoding.ASCII.GetBytes(body);
-            request.ContentLength = postBytes.Length;
-            using (var stream = request.GetRequestStream())
+            get
             {
-                stream.Write(postBytes, 0, postBytes.Length);
-                stream.Close();
+                return isConnectionError;
             }
-
-            var response = await request.GetResponseAsync() as HttpWebResponse;
-            using (var reader = new StreamReader(response.GetResponseStream()))
+            set
             {
-                return reader.ReadToEnd();
+                lock (Locker)
+                {
+                    isConnectionError = value;
+                }
+            }
+        }
+
+        public static async Task<HttpResult> PostAsync(string url, Dictionary<string, string> param)
+        {
+            try
+            {
+                var request = WebRequest.Create(url) as HttpWebRequest;
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.Timeout = 10000;
+                request.MaximumAutomaticRedirections = 5;
+                request.AllowAutoRedirect = true;
+                request.Headers.Add("Upgrade-Insecure-Requests", "1");
+
+                var body = string.Join("&", param.Select(p => $"{p.Key}={Uri.EscapeDataString(p.Value)}"));
+                var postBytes = Encoding.ASCII.GetBytes(body);
+                request.ContentLength = postBytes.Length;
+                using (var stream = request.GetRequestStream())
+                {
+                    stream.Write(postBytes, 0, postBytes.Length);
+                    stream.Close();
+                }
+
+                var response = await request.GetResponseAsync() as HttpWebResponse;
+
+                if (IsConnectionError)
+                {
+                    IsConnectionError = false;
+                }
+
+                using (var reader = new StreamReader(response.GetResponseStream()))
+                {
+                    return new HttpResult
+                    {
+                        Content = reader.ReadToEnd()
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                if (!isConnectionError)
+                {
+                    IsConnectionError = true;
+                }
+
+                return new HttpResult
+                {
+                    Exception = ex
+                };
             }
         }
     }
---
+++ TSensor.Proxy/HttpResult.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TSensor.Proxy
+{
+    public class HttpResult
+    {
+        public string Content { get; set; }
+        public Exception Exception { get; set; }
+    }
+}
