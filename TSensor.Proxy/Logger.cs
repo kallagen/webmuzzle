740f616a866c59b15d4693f417607d61ed02afbe
kirill <kirill.s.nikolaev@gmail.com>
2020-03-07 08:02:26 +0300

    add logger class with console output

---
+++ TSensor.Proxy/Logger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TSensor.Proxy
+{
+    public class Logger
+    {
+        private readonly object Locker = new object();
+
+        public void Write(object obj, Elapsed elapsed = null, string prefix = null)
+        {
+            lock (Locker)
+            {
+                var log = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
+                if (prefix != null)
+                {
+                    log += $" {prefix}";
+                }
+
+                log += $" {obj}";
+
+                if (elapsed != null)
+                {
+                    log += $" ({elapsed} ms)";
+                }
+
+                Console.WriteLine(log);
+            }
+        }
+    }
+}
