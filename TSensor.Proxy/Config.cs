e944a92f26494be9fd41674db8968e7f356b3727
kirill <kirill.s.nikolaev@gmail.com>
2020-03-08 07:15:53 +0300

    proxy: add archiveservice settings in config

--- TSensor.Proxy/Config.cs
+++ TSensor.Proxy/Config.cs
@@ -4,9 +4,12 @@ namespace TSensor.Proxy
 {
     public class Config
     {
-        public string ApiUrl { get; private set; }
+        public string ApiUrlSendValue { get; private set; }
+        public string ApiUrlSendArchive { get; private set; }
         public string DeviceGuid { get; private set; }
         public int CheckPortInterval { get; private set; }
+        public int ArchiveSendInterval { get; private set; }
+        public int MaxArchiveFileSize { get; private set; }
 
         public Config()
         {
@@ -14,9 +17,12 @@ namespace TSensor.Proxy
                 .AddJsonFile("appconfig.json")
                 .Build();
 
-            ApiUrl = $"http://{config["apiHost"]}/api/sensorvalue/push";
+            ApiUrlSendValue = $"http://{config["apiHost"]}/api/sensorvalue/push";
+            ApiUrlSendArchive = $"http://{config["apiHost"]}/api/sensorvalue/archive/push";
             DeviceGuid = config["deviceGuid"];
             CheckPortInterval = int.Parse(config["portCheckInterval"]) * 1000;
+            ArchiveSendInterval = int.Parse(config["archiveSendInterval"]) * 1000;
+            MaxArchiveFileSize = int.Parse(config["maxArchiveFileSize"]) * 1024;
         }
     }
 }
