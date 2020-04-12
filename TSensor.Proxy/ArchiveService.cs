3e3fd41493be2c5d280028e1b506484d8acf8513
kirill <kirill.s.nikolaev@gmail.com>
2020-04-12 15:15:54 +0300

    rework archive service switch from timer to backgroundworker

--- TSensor.Proxy/ArchiveService.cs
+++ TSensor.Proxy/ArchiveService.cs
@@ -1,28 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Threading;
+using System.Threading.Tasks;
+using TSensor.Proxy.Logger;
 
 namespace TSensor.Proxy
 {
     public class ArchiveService
     {
         private readonly Config _config;
-        private readonly Logger _logger;
+        private readonly ILogger _logger;
+
+        private readonly BackgroundWorker worker;
 
         private readonly object Locker = new object();
-        private bool IsSendRunning = false;
-
         private const string FILE_NAME = "current.archived";
 
-        private readonly Timer timer;
-
-        public ArchiveService(Config config, Logger logger)
+        public ArchiveService(Config config, ILogger logger)
         {
             _config = config;
             _logger = logger;
 
-            timer = new Timer(new TimerCallback(Send), null, 0, _config.ArchiveSendInterval);
+            worker = new BackgroundWorker();
+            worker.DoWork += Worker_DoWork;
+            worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
+        }
+
+        private async void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            await SendAsync();
+        }
+
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            Thread.Sleep(_config.ArchiveSendInterval);
+
+            worker.RunWorkerAsync();
+        }
+
+        public void Run()
+        {
+            worker.RunWorkerAsync();
         }
 
         public void Write(string portName, string data)
@@ -33,15 +53,15 @@ namespace TSensor.Proxy
                 {
                     File.AppendAllLines(FILE_NAME, new[] { data });
 
-                    _logger.Write($"{portName}: data archived");
+                    _logger.Log("data archived", prefix: portName);
                 }
 
                 FinishCurrentArchive();
             }
             catch (Exception ex)
             {
-                _logger.Write($"{portName}: data archiving error");
-                _logger.Write(ex);
+                _logger.Log("data archiving error", prefix: portName);
+                _logger.Log(ex.Message);
             }
         }
 
@@ -56,93 +76,77 @@ namespace TSensor.Proxy
                         var file = new FileInfo(FILE_NAME);
                         if (file.Length > _config.MaxArchiveFileSize || forceMove)
                         {
-                            var newFileName = $"{DateTime.Now.ToString("yyyyMMddHHmmss")}.archived";
+                            var newFileName = $"{DateTime.Now:yyyyMMddHHmmss}.archived";
                             file.MoveTo(newFileName);
 
-                            _logger.Write($"archive moved to {newFileName}");
+                            _logger.Log($"archive moved to {newFileName}");
                         }
                     }
                 }
                 catch (Exception ex)
                 {
-                    _logger.Write($"current.archive moving error");
-                    _logger.Write(ex);
+                    _logger.Log($"current.archive moving error");
+                    _logger.Log(ex.Message);
                 }
             }
         }
-        private async void Send(object state)
+
+        private async Task SendAsync()
         {
-            if (IsSendRunning)
+            if (Http.Http.IsConnectionError)
             {
-                _logger.Write("archive service already running, skip sending");
+                _logger.Log("last data sending return connection error, ignore archive sending");
+
+                return;
             }
-            else
+
+            FinishCurrentArchive(forceMove: true);
+
+            try
             {
-                IsSendRunning = true;
-
-                if (!Http.IsConnectionError)
+                foreach (var archive in Directory.GetFiles(Directory.GetCurrentDirectory(), "*.archived"))
                 {
-                    FinishCurrentArchive(forceMove: true);
-
-                    try
+                    var fileName = Path.GetFileName(archive);
+                    if (fileName != FILE_NAME)
                     {
-                        foreach (var archive in Directory.GetFiles(Directory.GetCurrentDirectory(), "*.archived"))
-                        {
-                            var fileName = Path.GetFileName(archive);
-                            if (fileName != FILE_NAME)
+                        using var elapsed = Elapsed.Create;
+
+                        var result = await Http.Http.PostAsync(_config.ApiUrlSendArchive,
+                            new Dictionary<string, string>
                             {
-                                using (var elapsed = Elapsed.Create)
-                                {
-                                    var result = await Http.PostAsync(_config.ApiUrlSendArchive, new Dictionary<string, string>
-                                    {
-                                        { "d", _config.DeviceGuid },
-                                        { "a", await File.ReadAllTextAsync(archive) }
-                                    });
+                                { "d", _config.DeviceGuid },
+                                { "a", await File.ReadAllTextAsync(archive) }
+                            });
 
-                                    if (result.Exception != null)
-                                    {
-                                        _logger.Write($"sending archive {fileName} error");
-                                        _logger.Write(result.Exception);
-                                    }
-                                    else
-                                    {
-                                        _logger.Write($"archive {fileName} sended", elapsed);
-                                        _logger.Write($"response: {result.Content}");
+                        if (result.Exception != null)
+                        {
+                            _logger.Log($"sending archive {fileName} error");
+                            _logger.Log(result.Exception.Message);
+                        }
+                        else
+                        {
+                            _logger.Log($"archive {fileName} sended({result.Content})", elapsed: elapsed);
 
-                                        try
-                                        {
-                                            File.Delete(archive);
+                            try
+                            {
+                                File.Delete(archive);
 
-                                            _logger.Write($"archive {fileName} removed");
-                                        }
-                                        catch (Exception ex)
-                                        {
-                                            _logger.Write($"removing archive {fileName} error");
-                                            _logger.Write(ex);
-                                        }
-                                    }
-                                }
+                                _logger.Log($"archive {fileName} removed");
+                            }
+                            catch (Exception ex)
+                            {
+                                _logger.Log($"removing archive {fileName} error");
+                                _logger.Log(ex.Message);
                             }
                         }
                     }
-                    catch (Exception ex)
-                    {
-                        _logger.Write($"archive service working error");
-                        _logger.Write(ex);
-                    }
                 }
-                else
-                {
-                    _logger.Write("last post return connection error, ignore archive sending");
-                }
-
-                IsSendRunning = false;
             }
-        }
-
-        public void Run()
-        {
-            while (true) { }
+            catch (Exception ex)
+            {
+                _logger.Log($"archive service working error");
+                _logger.Log(ex.Message);
+            }
         }
     }
 }
