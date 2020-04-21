13850c5ad203ac75cafda90675146bcbf8786e82
kirill <kirill.s.nikolaev@gmail.com>
2020-04-21 16:29:21 +0300

    fix issue with chart artefacts

--- TSensor.Web/Controllers/ChartController.cs
+++ TSensor.Web/Controllers/ChartController.cs
@@ -62,7 +62,8 @@ namespace TSensor.Web.Controllers
                             y = p.Value
                         })
                         .GroupBy(p => p.x)
-                        .Select(p => new { x = p.Key, y = Math.Round(p.Average(v => (decimal)v.y), 3) }),
+                        .Select(p => new { x = p.Key, y = Math.Round(p.Average(v => (decimal)v.y), 3) })
+                        .OrderBy(p => p.x),
                     backgroundColor = dataset.Key.isSecond ? "#4BC0C0" : "#36A2EB",
                     borderColor = dataset.Key.isSecond ? "#4BC0C0" : "#36A2EB",
                     yAxisID = dataset.Key.isSecond ? "y-axis-additional" : "y-axis-main",
