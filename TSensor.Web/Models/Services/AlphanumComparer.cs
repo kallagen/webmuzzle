fd6ee5fe5494a558401381df152fd41d5944068e
kirill <kirill.s.nikolaev@gmail.com>
2020-04-30 08:18:21 +0300

    add alphanumeral comparer for sort

---
+++ TSensor.Web/Models/Services/AlphanumComparer.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace TSensor.Web.Models.Services
+{
+    public class AlphanumComparer : IComparer<string>
+    {
+        private enum ChunkType { Alphanumeric, Numeric };
+
+        private bool InChunk(char ch, char otherCh)
+        {
+            var type = char.IsDigit(otherCh) ? ChunkType.Numeric :
+                ChunkType.Alphanumeric;
+
+            return !((type == ChunkType.Alphanumeric && char.IsDigit(ch))
+                || (type == ChunkType.Numeric && !char.IsDigit(ch)));
+        }
+
+        public int Compare(string s1, string s2)
+        {
+            if (s1 == null || s2 == null)
+            {
+                return 0;
+            }
+
+            int thisMarker = 0, thisNumericChunk;
+            int thatMarker = 0, thatNumericChunk;
+
+            while ((thisMarker < s1.Length) || (thatMarker < s2.Length))
+            {
+                if (thisMarker >= s1.Length)
+                {
+                    return -1;
+                }
+                else if (thatMarker >= s2.Length)
+                {
+                    return 1;
+                }
+
+                char thisCh = s1[thisMarker];
+                char thatCh = s2[thatMarker];
+
+                var thisChunk = new StringBuilder();
+                var thatChunk = new StringBuilder();
+
+                while ((thisMarker < s1.Length) && (thisChunk.Length == 0 || InChunk(thisCh, thisChunk[0])))
+                {
+                    thisChunk.Append(thisCh);
+                    thisMarker++;
+
+                    if (thisMarker < s1.Length)
+                    {
+                        thisCh = s1[thisMarker];
+                    }
+                }
+
+                while ((thatMarker < s2.Length) && (thatChunk.Length == 0 || InChunk(thatCh, thatChunk[0])))
+                {
+                    thatChunk.Append(thatCh);
+                    thatMarker++;
+
+                    if (thatMarker < s2.Length)
+                    {
+                        thatCh = s2[thatMarker];
+                    }
+                }
+
+                var result = 0;
+
+                if (char.IsDigit(thisChunk[0]) && char.IsDigit(thatChunk[0]))
+                {
+                    thisNumericChunk = Convert.ToInt32(thisChunk.ToString());
+                    thatNumericChunk = Convert.ToInt32(thatChunk.ToString());
+
+                    if (thisNumericChunk < thatNumericChunk)
+                    {
+                        result = -1;
+                    }
+
+                    if (thisNumericChunk > thatNumericChunk)
+                    {
+                        result = 1;
+                    }
+                }
+                else
+                {
+                    result = thisChunk.ToString().CompareTo(thatChunk.ToString());
+                }
+
+                if (result != 0)
+                {
+                    return result;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
