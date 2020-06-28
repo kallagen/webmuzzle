3c22497d06ad17226b07e7b61400ca5fef20bfd2
kirill <kirill.s.nikolaev@gmail.com>
2020-06-28 18:41:37 +0300

    add logging in system error page redirect

--- TSensor.Web/Controllers/SystemController.cs
+++ TSensor.Web/Controllers/SystemController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TSensor.Web.Models.Services.Log;
 
 namespace TSensor.Web.Controllers
 {
     public class SystemController : Controller
     {
+        private readonly FileLogService _logService;
+
+        public SystemController(FileLogService logService)
+        {
+            _logService = logService;
+        }
+
         [Route("error")]
         public IActionResult Error()
         {
+            var error =
+                HttpContext.Features.Get<IExceptionHandlerPathFeature>()?.Error;
+            if (error != null)
+            {
+                _logService.Write("systemexception", error.Message);
+            }
+
             return View();
         }
     }
