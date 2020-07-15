bdaeeea94698899f8a86e1a9f3c2a19f0ae821da
kirill <kirill.s.nikolaev@gmail.com>
2020-07-15 13:06:00 +0300

    license: add install sql script

---
+++ TSensor.License/Install.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE License
+(
+	LicenseGuid UNIQUEIDENTIFIER PRIMARY KEY,
+	Data nvarchar(MAX),
+	Name nvarchar(128),
+	ExpireDate date NOT NULL,
+	SensorCount int NOT NULL,
+	CreationDateUTC datetime NOT NULL DEFAULT GETUTCDATE(),
+	IsActivated bit NOT NULL DEFAULT 0
+)
+GO
+
+CREATE TABLE LicenseActivation
+(
+	LicenseGuid uniqueidentifier FOREIGN KEY REFERENCES License(LicenseGuid) ON DELETE NO ACTION,
+	ActivationDateUTC datetime NOT NULL DEFAULT GETUTCDATE(),
+	ActivationIp nvarchar(16)
+)
+GO
