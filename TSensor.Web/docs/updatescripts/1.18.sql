53417d0285c739e9344b84b4337c2bcc2d4073e0
kirill <kirill.s.nikolaev@gmail.com>
2020-08-25 10:47:12 +0300

    add 1.18 upgrade script

---
+++ TSensor.Web/docs/updatescripts/1.18.sql
@@ -0,0 +1,17 @@
+﻿ALTER TABLE Product Add IsGas bit NOT NULL DEFAULT 0
+GO
+
+UPDATE ActualSensorValue SET pressureMeasuring = 0
+GO
+ALTER TABLE ActualSensorValue ALTER COLUMN pressureMeasuring DECIMAL(10, 2) NULL
+GO
+
+UPDATE SensorValue SET pressureMeasuring = 0
+GO
+ALTER TABLE SensorValue ALTER COLUMN pressureMeasuring DECIMAL(10, 2) NULL
+GO
+
+ALTER TABLE Point ADD Longitude decimal(10, 6)
+GO
+ALTER TABLE Point ADD Latitude decimal(10, 6)
+GO
