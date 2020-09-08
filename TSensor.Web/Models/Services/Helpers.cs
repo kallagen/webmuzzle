6b6f5831a33ef24d6ff720264eed4b0ea1103cdd
kirill <kirill.s.nikolaev@gmail.com>
2020-09-08 09:00:30 +0300

    core: add try parse decimal helper

--- TSensor.Web/Models/Services/Helpers.cs
+++ TSensor.Web/Models/Services/Helpers.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace TSensor.Web.Models.Services
 {
@@ -7,6 +8,26 @@ namespace TSensor.Web.Models.Services
         public static long TicksJs(this DateTime date)
         {
             return (date.Ticks - 621355968000000000) / 10000;
+        }
+
+        public static bool TryParseDecimal(this string str, out decimal result)
+        {
+            result = 0;
+
+            switch (CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator)
+            {
+                case ".": str = str?.Replace(",", "."); break;
+                case ",": str = str?.Replace(".", ","); break;
+                default: break;
+            }
+
+            var parseResult = decimal.TryParse(str, out var _result);
+            if (parseResult)
+            {
+                result = _result;
+            }
+
+            return parseResult;
         }
     }
 }
