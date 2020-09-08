1d15314917e454e37c7c20bc815a789fc12cbc9a
kirill <kirill.s.nikolaev@gmail.com>
2020-09-08 09:42:55 +0300

    1.22 + updatescript

---
+++ TSensor.Web/docs/updatescripts/1.22.sql
@@ -0,0 +1,12 @@
+﻿ALTER TABLE Point ADD CoordinatesChanged bit DEFAULT 0
+GO
+
+CREATE TABLE PointCoordinates
+(
+	PointGuid uniqueidentifier FOREIGN KEY REFERENCES Point(PointGuid) ON DELETE CASCADE,
+	ActualDate datetime,
+	Longitude decimal(10, 6),
+	Latitude decimal(10, 6),
+	PRIMARY KEY(PointGuid, ActualDate)
+)
+GO
