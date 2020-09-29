26cfa69a57ead1b507f824f642c0b23e3e78c1aa
kirill <kirill.s.nikolaev@gmail.com>
2020-09-29 08:11:32 +0300

    fake: add sync post method

--- TSensor.FakeSensor/Http.cs
+++ TSensor.FakeSensor/Http.cs
@@ -49,5 +49,45 @@ namespace TSensor.FakeSensor
                 response = null;
             }
         }
+
+        public static string Post(string url, Dictionary<string, string> param)
+        {
+            HttpWebRequest request;
+            HttpWebResponse response;
+
+            try
+            {
+                request = WebRequest.Create(url) as HttpWebRequest;
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.Timeout = 10000;
+                request.MaximumAutomaticRedirections = 5;
+                request.AllowAutoRedirect = true;
+                request.Headers.Add("Upgrade-Insecure-Requests", "1");
+
+                var body = string.Join("&", param.Select(p => $"{p.Key}={WebUtility.UrlEncode(p.Value)}"));
+                var postBytes = Encoding.ASCII.GetBytes(body);
+                request.ContentLength = postBytes.Length;
+                using (var stream = request.GetRequestStream())
+                {
+                    stream.Write(postBytes, 0, postBytes.Length);
+                    stream.Close();
+                }
+
+                response = request.GetResponse() as HttpWebResponse;
+                using var reader = new StreamReader(response.GetResponseStream());
+
+                return reader.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            finally
+            {
+                request = null;
+                response = null;
+            }
+        }
     }
 }
