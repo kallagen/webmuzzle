93fa3c70f7a2b9b2938b3d5fdd35cadd196477e6
kirill <kirill.s.nikolaev@gmail.com>
2020-10-27 22:21:30 +0300

    ver 1.26 + scripts

---
+++ TSensor.Web/docs/updatescripts/1.26.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE PointType
+(
+	PointTypeGuid uniqueidentifier PRIMARY KEY DEFAULT NEWID(),
+	Name nvarchar(128) NOT NULL,
+	[Image] nvarchar(MAX) DEFAULT NULL
+)
+GO
+
+ALTER TABLE [Point] ADD PointTypeGuid uniqueidentifier FOREIGN KEY REFERENCES PointType(PointTypeGuid) ON DELETE SET NULL
+GO
