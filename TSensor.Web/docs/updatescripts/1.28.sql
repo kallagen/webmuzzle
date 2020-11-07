5620ba61f904de3ce0ae048f04961119c89da365
kirill <kirill.s.nikolaev@gmail.com>
2020-11-07 11:10:18 +0300

    1.28 + scripts

---
+++ TSensor.Web/docs/updatescripts/1.28.sql
@@ -0,0 +1,15 @@
+﻿ALTER TABLE Point ADD IsMoving bit
+GO
+ALTER TABLE Point ADD LastMovingDate datetime 
+GO
+
+DROP TABLE PointCoordinates
+GO
+
+CREATE TABLE PointCoordinates (
+    PointGuid UNIQUEIDENTIFIER FOREIGN KEY REFERENCES Point(PointGuid) ON DELETE CASCADE NOT NULL,
+    ActualDate DATETIME DEFAULT (getdate()) NOT NULL,
+    Longitude DECIMAL(10, 6) NOT NULL,
+    Latitude DECIMAL(10, 6) NOT NULL
+)
+GO
